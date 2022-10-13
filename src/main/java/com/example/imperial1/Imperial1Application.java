3299ae11c0ae3744d2277bef24a7415fd2dc18a7
Leon <davidyu362000@gmail.com>
2022-10-13 22:45:00 +0200

    Initial commit

---
+++ src/main/java/com/example/imperial1/Imperial1Application.java
@@ -0,0 +1,13 @@
+package com.example.imperial1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Imperial1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Imperial1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imperial1/Imperial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imperial1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Imperial1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
